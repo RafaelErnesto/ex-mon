5901acc5dafe9db8698ee228264534bf16bb3308
Rafael <rflernesto@gmail.com>
2022-05-23 21:26:26 -0400

    feat: fetch move

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Player, Game}
+  alias ExMon.Game.{Status, Actions}
   @computer_name "Robot"
 
   def create_player(name, average_move, random_move, healing_move) do
@@ -9,5 +10,11 @@ defmodule ExMon do
   def start_game(player) do
     create_player(@computer_name, :punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
--- lib/player.ex
+++ lib/player.ex
@@ -1,5 +1,5 @@
 defmodule ExMon.Player do
-  @required_keys [:life, :name, :random_move, :average_move, :healing_move]
+  @required_keys [:life, :name, :moves]
   @max_life 100
 
   @enforce_keys @required_keys
@@ -8,9 +8,11 @@ defmodule ExMon.Player do
   def build(name, random_move, average_move, healing_move) do
     %ExMon.Player{
       name: name,
-      random_move: random_move,
-      average_move: average_move,
-      healing_move: healing_move,
+      moves: %{
+        random_move: random_move,
+        average_move: average_move,
+        healing_move: healing_move,
+      },
       life: @max_life
     }
   end
