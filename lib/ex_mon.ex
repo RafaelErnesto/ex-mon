2e09dc135ebf4c214f8bcadfcc138da1fd75c727
Rafael <rflernesto@gmail.com>
2022-05-23 20:57:15 -0400

    feat: create agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Player, Game}
+  @computer_name "Robot"
 
   def create_player(name, average_move, random_move, healing_move) do
     Player.build(name, random_move, average_move, healing_move)
+  end
+
+  def start_game(player) do
+    create_player(@computer_name, :punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
